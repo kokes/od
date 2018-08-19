90faefebd240203e795789d13dbc821e4bfb0585
Ondrej Kokes <ondrej.kokes@gmail.com>
2018-08-19 14:23:34 +0200

    [ares/res] iterativni stahovani

--- data/ares/api/dl.py
+++ data/ares/api/dl.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from urllib.request import urlopen
 
 import psycopg2
+from tqdm import tqdm
 
 def ares_url(rejstrik, ico):
     if rejstrik == 'or':
@@ -11,29 +12,31 @@ def ares_url(rejstrik, ico):
 
     return 'http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_{}.cgi?ico={}'.format(rejstrik, ico)
 
+if __name__ == '__main__':
+    rejstrik = sys.argv[1]
+    assert rejstrik in ['res', 'or']
 
-rejstrik = sys.argv[1]
-assert rejstrik in ['res', 'or']
+    while True:
+        with psycopg2.connect(host='localhost') as conn, conn.cursor() as cursor:
+            cursor.execute('select ico from od.ares_raw where rejstrik = %s and xml is null limit 1000',
+                       (rejstrik,))
+            icos = [j[0] for j in cursor.fetchall()]
+            if len(icos) == 0: break
 
-conn = psycopg2.connect(host='localhost')
-with conn, conn.cursor() as cursor:
-    cursor.execute('select ico from od.ares_raw where rejstrik = %s and xml is null order by ico asc limit 250000',
-                   (rejstrik,)) # TODO: order by modified_on asc (and found is not false?)
-    icos = [j[0] for j in cursor.fetchall()]
+            for ico in tqdm(icos):
+                assert isinstance(ico, int) and ico > 0 and len(str(ico)) <= 8, 'invalid format, {}'.format(ico)
+                r = urlopen(ares_url(rejstrik, ico))
+                dt = r.read()
+                if b'Chyba 23 - chybn' in dt:
+                    raise ValueError('nespravny format ico: {}'.format(ico))
 
-for j, ico in enumerate(icos):
-    print('\t{}/{}'.format(j+1, len(icos)), end='\r')
-    assert isinstance(ico, int) and ico > 0 and len(str(ico)) <= 8, 'invalid format, {}'.format(ico)
-    r = urlopen(ares_url(rejstrik, ico))
-    dt = r.read()
-    if b'Chyba 23 - chybn' in dt:
-        raise ValueError('nespravny format ico: {}'.format(ico))
+                if b'<dtt:faultcode>' in dt:
+                    raise ValueError('chyba v API')
 
-    found = b'Chyba 71 - nenalezeno' not in dt
-    if not found:
-        print(ico, 'nenalezeno')
+                found = b'Chyba 71 - nenalezeno' not in dt
+                if not found:
+                    print(ico, 'nenalezeno')
 
-    with conn, conn.cursor() as cursor:
-        # TODO: upsert? (kdybychom meli ICO odjinud)
-        cursor.execute('update od.ares_raw set modified_on=%s, xml=%s, found=%s where rejstrik = %s and ico = %s',
-            (datetime.utcnow(), dt, found, rejstrik, ico))
+                # TODO: upsert? (kdybychom meli ICO odjinud)
+                cursor.execute('update od.ares_raw set modified_on=%s, xml=%s, found=%s where rejstrik = %s and ico = %s',
+                    (datetime.utcnow(), dt, found, rejstrik, ico))
