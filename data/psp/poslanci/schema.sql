9c2dc618351dc6ab56065ff1f049d74b655ae3e7
Ondrej Kokes <ondrej.kokes@gmail.com>
2018-09-26 14:31:54 +0200

    [psp] minoritni uprava schemat tabulek
    Cizi klice jsem pridal jen jako komentare, protoze integrita je porusena uz behem insertu, museli bychom je pridat pote.

--- data/psp/poslanci/schema.sql
+++ data/psp/poslanci/schema.sql
@@ -1,18 +1,19 @@
-DROP TABLE IF EXISTS psp_typ_organu;
+BEGIN;
+DROP TABLE IF EXISTS psp_typ_organu CASCADE;
 CREATE TABLE psp_typ_organu (
-	"id_typ_org" int,
-	"typ_id_typ_org" int,
+	"id_typ_org" int unique not null,
+	"typ_id_typ_org" int, -- REFERENCES psp_typ_organu(id_typ_org)
 	"nazev_typ_org_cz" varchar,
 	"nazev_typ_org_en" varchar,
-	"typ_org_obecny" int,
+	"typ_org_obecny" int, -- REFERENCES psp_typ_organu(id_typ_org)
 	"priorita" int,
 	"x" varchar
 );
 
-DROP TABLE IF EXISTS psp_typ_funkce;
+DROP TABLE IF EXISTS psp_typ_funkce CASCADE;
 CREATE TABLE psp_typ_funkce (
-	"id_typ_funkce" int,
-	"id_typ_org" int,
+	"id_typ_funkce" int unique not null,
+	"id_typ_org" int, -- REFERENCES psp_typ_organu(id_typ_org)
 	"typ_funkce_cz" varchar,
 	"typ_funkce_en" varchar,
 	"priorita" int,
@@ -20,21 +21,11 @@ CREATE TABLE psp_typ_funkce (
 	"x" varchar
 );
 
-DROP TABLE IF EXISTS psp_funkce;
-CREATE TABLE psp_funkce (
-	"id_funkce" int,
-	"id_organ" int,
-	"id_typ_funkce" int,
-	"nazev_funkce_cz" varchar,
-	"priorita" int,
-	"x" varchar
-);
-
-DROP TABLE IF EXISTS psp_organy;
+DROP TABLE IF EXISTS psp_organy CASCADE;
 CREATE TABLE psp_organy (
-	"id_organ" int,
+	"id_organ" int unique not null,
 	"organ_id_organ" int,
-	"id_typ_organu" int,
+	"id_typ_organu" int, -- REFERENCES psp_typ_organu(id_typ_org)
 	"zkratka" varchar,
 	"nazev_organu_cz" varchar,
 	"nazev_organu_en" varchar,
@@ -45,9 +36,19 @@ CREATE TABLE psp_organy (
 	"x" varchar
 );
 
-DROP TABLE IF EXISTS psp_osoby;
+DROP TABLE IF EXISTS psp_funkce CASCADE;
+CREATE TABLE psp_funkce (
+	"id_funkce" int unique not null,
+	"id_organ" int, -- REFERENCES psp_organy(id_organ)
+	"id_typ_funkce" int, -- REFERENCES psp_typ_funkce(id_typ_funkce)
+	"nazev_funkce_cz" varchar,
+	"priorita" int,
+	"x" varchar
+);
+
+DROP TABLE IF EXISTS psp_osoby CASCADE;
 CREATE TABLE psp_osoby (
-	"id_osoba" int,
+	"id_osoba" int unique not null,
 	"pred" varchar,
 	"prijmeni" varchar,
 	"jmeno" varchar,
@@ -59,10 +60,10 @@ CREATE TABLE psp_osoby (
 	"x" varchar
 );
 
-DROP TABLE IF EXISTS psp_zarazeni;
+DROP TABLE IF EXISTS psp_zarazeni CASCADE;
 CREATE TABLE psp_zarazeni (
-	"id_osoba" int,
-	"id_of" int,
+	"id_osoba" int, -- REFERENCES psp_osoby(id_osoba)
+	"id_of" int, -- dve ruzne reference podle hodnoty `cl_funkce`
 	"cl_funkce" int,
 	"od_o" varchar, -- TODO: jsou tam i hodiny, je to takovy pseudoformat
 	"do_o" varchar, -- TODO: jsou tam i hodiny, je to takovy pseudoformat
@@ -71,13 +72,13 @@ CREATE TABLE psp_zarazeni (
 	"x" varchar
 );
 
-DROP TABLE IF EXISTS psp_poslanec;
+DROP TABLE IF EXISTS psp_poslanec CASCADE;
 CREATE TABLE psp_poslanec (
-	"id_poslanec" int,
-	"id_osoba" int,
-	"id_kraj" int,
-	"id_kandidatka" int,
-	"id_obdobi" int,
+	"id_poslanec" int unique not null,
+	"id_osoba" int, -- REFERENCES psp_osoby(id_osoba)
+	"id_kraj" int, -- REFERENCES psp_organy(id_organ)
+	"id_kandidatka" int, -- REFERENCES psp_organy(id_organ)
+	"id_obdobi" int, -- REFERENCES psp_organy(id_organ)
 	"web" varchar,
 	"ulice" varchar,
 	"obec" varchar,
@@ -91,11 +92,12 @@ CREATE TABLE psp_poslanec (
 	"x" varchar
 );
 
-DROP TABLE IF EXISTS psp_pkgps;
+DROP TABLE IF EXISTS psp_pkgps CASCADE;
 CREATE TABLE psp_pkgps (
-	"id_poslanec" int,
+	"id_poslanec" int, -- REFERENCES psp_poslanec(id_poslanec)
 	"adresa" varchar,
-	"sirka" varchar,
-	"delka" varchar,
+	"sirka" numeric(9, 2),
+	"delka" numeric(9, 2),
 	"x" varchar
 );
+COMMIT;
