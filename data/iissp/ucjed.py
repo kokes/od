911caf794682fb74212b2eb67b0e6288a43aabe6
Ondrej Kokes <ondrej.kokes@gmail.com>
2020-12-18 14:23:57 +0100

    aktualizace formátu iissp dat (monitor státní pokladny)

--- data/iissp/ucjed.py
+++ data/iissp/ucjed.py
@@ -4,10 +4,10 @@ from urllib.request import urlopen
 
 import lxml.etree
 
-url = 'https://monitor.statnipokladna.cz/data/ucjed.xml'
+url = 'https://monitor.statnipokladna.cz/data/xml/ucjed.xml'
 
 # XSD nema vsechno, dafuq
-cols = 'ico, start_date, end_date, ucjed_nazev, dic, adresa, nuts_id, zrizovatel_ico, cofog_id, isektor_id, kapitola_id, nace_id, druhuj_id, poddruhuj_id, konecplat, forma_id, katobyv_id, obec, kraj, stat_id, zdrojfin_id, druhrizeni_id, veduc_id, zuj, sidlo, zpodm_id, kod_pou, typorg_id, pocob, ulice, kod_rp, datumakt, aktorg_id, datumvzniku, psc'.split(
+cols = 'csuis_ucjed_id, ico, start_date, end_date, ucjed_nazev, dic, adresa, nuts_id, zrizovatel_id, cofog_id, isektor_id, kapitola_id, nace_id, druhuj_id, poddruhuj_id, konecplat, forma_id, katobyv_id, stat_id, zdrojfin_id, druhrizeni_id, veduc_id, zuj, sidlo, zpodm_id, kod_pou, typorg_id, pocob, ulice, kod_rp, datumakt, aktorg_id, datumvzniku, psc, pou_id, orp_id, zuj_id'.split(
     ', ')
 
 dates = 'start_date, end_date, konecplat, datumakt, datumvzniku'.split(', ')
@@ -21,7 +21,7 @@ with urlopen(url) as f, open('ucjed.csv', 'w', encoding='utf8') as fw:
     for action, element in et:
         assert action == 'end', action
 
-        if element.tag != 'Radek':
+        if element.tag != 'row':
             continue
 
         row = {j.tag: j.text for j in element.getchildren()}
@@ -32,12 +32,17 @@ with urlopen(url) as f, open('ucjed.csv', 'w', encoding='utf8') as fw:
             continue
 
         for datecol in dates:
-            if row[datecol]:
-                day, month, year = row[datecol][:2], row[datecol][3:5], row[datecol][6:]
-                row[datecol] = date(int(year), int(month), int(day))
+            if (not row[datecol] or row[datecol] == '00000000'):
+                row[datecol] = None
+                continue
 
-        if row['zrizovatel_ico'] == 'Chybí':
-            row['zrizovatel_ico'] = ''
+            if '-' in row[datecol]:
+                # 21-11-2019
+                day, month, year = row[datecol][:2], row[datecol][3:5], row[datecol][6:]
+            else:
+                # 20191121
+                day, month, year = row[datecol][6:], row[datecol][4:6], row[datecol][:4]
+            row[datecol] = date(int(year), int(month), int(day))
 
         cw.writerow(row)
         element.clear()
--- data/iissp/ucjed.sql
+++ data/iissp/ucjed.sql
@@ -1,28 +1,28 @@
 CREATE SCHEMA IF NOT EXISTS iissp;
+DROP TABLE IF EXISTS iissp.ucetni_jednotky;
 CREATE TABLE iissp.ucetni_jednotky (
+       csuis_ucjed_id bigint not null,
        ico int not null,
        start_date date not null,
        end_date date not null,
        ucjed_nazev text, -- fakt to tam obcas chybi
        dic text,
        adresa text not null,
-       nuts_id text not null,
-       zrizovatel_ico int,
+       nuts_id text,
+       zrizovatel_id bigint, -- references(csuis_ucjed_id),
        cofog_id smallint, -- nullif == 0?
        isektor_id smallint, -- nullif == 0?
        kapitola_id smallint, -- nullif == 0?
-       nace_id int not null,
-       druhuj_id smallint not null,
-       poddruhuj_id smallint not null,
+       nace_id int,
+       druhuj_id smallint,
+       poddruhuj_id smallint,
        konecplat date,
-       forma_id smallint not null,
+       forma_id text, -- ma byt short/int, ale občas to má jiný hodnoty
        katobyv_id smallint not null,
-       obec text,
-       kraj text not null,
        stat_id smallint,
        zdrojfin_id smallint,
        druhrizeni_id smallint,
-       veduc_id smallint not null,
+       veduc_id smallint,
        zuj int,
        sidlo text,
        zpodm_id smallint,
@@ -31,8 +31,11 @@ CREATE TABLE iissp.ucetni_jednotky (
        pocob int,
        ulice text,
        kod_rp text,
-       datumakt date not null,
+       datumakt date,
        aktorg_id smallint,
-       datumvzniku date not null,
-       psc text
+       datumvzniku date,
+       psc text,
+       pou_id int,
+       orp_id int,
+       zuj_id int
 );
