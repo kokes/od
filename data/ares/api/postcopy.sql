7a6ad7842131662593a160ac0afc5f0492c2fb7b
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-02-02 22:00:14 +0100

    [ares] cas odstranit API pristup
    Uplne prvni komponenta, slouzilo to dobre cely ty roky, ale mame
    bulkovej pristup, kterej ma sice min dat, ale je skoro bezudrzbovej a
    bezstavovej.

--- data/ares/api/postcopy.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ares.or_angos_fo ADD COLUMN jmeno_prijmeni varchar;
-UPDATE ares.or_angos_fo SET jmeno_prijmeni = LOWER(TRIM(CONCAT(jmeno, ' ', prijmeni)));
-CREATE INDEX ares_or_angos_fo_jmeno_prijmeni_idx ON ares.or_angos_fo(jmeno_prijmeni);
