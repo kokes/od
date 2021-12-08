74a6267805b65cbd6694334ffc041e9bbae54a29
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-12-08 15:34:44 +0100

    tried datetime parsing, but it's not feasible

--- data/icij/main.py
+++ data/icij/main.py
@@ -1,5 +1,8 @@
+import csv
 import os
 import shutil
+import datetime as dt
+from io import TextIOWrapper
 from contextlib import closing
 from tempfile import NamedTemporaryFile
 from urllib.request import urlopen
@@ -7,20 +10,50 @@ from zipfile import ZipFile
 
 url = "https://offshoreleaks-data.icij.org/offshoreleaks/csv/full-oldb-20211202.zip"
 
+# convert a date-like string into an ISO-8601-formatted string
+def isofy(v: str) -> str:
+    # convert 06-JAN-2006 into ISO-8601
+    if v[2] == "-":
+        return dt.datetime.strptime(v, r"%d-%b-%Y").date().isoformat()
+    # ... or "Sep 25, 2012"
+    elif "," in v:
+        return dt.datetime.strptime(v, r"%b %d, %Y").date().isoformat()
+    elif len(v) == 4 and v.isdigit():
+        return f"{v}-01-01" # simplify year-only dates to be Jan 1
+    # ... or 04/27/05
+    elif len(v) == 8 and v[2] == "/":
+        pass # cannot implement this now, because these are ambiguous
+    else:
+        # now we assume it's ISO-8601, which is fine
+        dt.date.fromisoformat(v)
+        return v
+    
 
 def main(outdir: str, partial: bool = False):
-    tmpf = NamedTemporaryFile()
-    with closing(urlopen(url)) as rr:
-        shutil.copyfileobj(rr, tmpf)
+    # tmpf = NamedTemporaryFile()
+    # with closing(urlopen(url)) as rr:
+    #     shutil.copyfileobj(rr, tmpf)
 
-    with ZipFile(tmpf.name) as zf:
+    # with ZipFile(tmpf.name) as zf:
+    with ZipFile("full-oldb-20211202.zip") as zf:
         for member in zf.filelist:
-            zf.extract(member, path=outdir)
-            if member.filename.startswith("nodes-"):
-                target = os.path.join(outdir, member.filename)
-                new_target = target.replace("nodes-", "")
-                os.rename(target, new_target)
+            with zf.open(member) as f:
+                print(f"processing {member.filename}")
+                tr = TextIOWrapper(f)
+                cr = csv.DictReader(tr)
+                target = os.path.join(outdir, member.filename.replace("nodes-", ""))
+                # this is now slightly redundant, because we could just use .extract as before,
+                # but we have this in place in case ICIJ fixes their date issues
+                with open(target, "wt", encoding="utf-8") as fw:
+                    cw = csv.DictWriter(fw, fieldnames=cr.fieldnames)
+                    cw.writeheader()
+                    for line in cr:
+                        # cannot use this now because there are many many ambiguities
+                        # for k, v in line.items():
+                        #     if k.endswith("_date") and v:
+                        #         line[k] = isofy(v)
+                        cw.writerow(line)
 
 
 if __name__ == "__main__":
-    main(".")
+    main("csv") # TODO(PR): fix
---
+++ data/icij/test_isofy.py
@@ -0,0 +1,17 @@
+# from main import isofy
+
+# errors:
+# 26/06/0206, 18/19/2015, 01.004.04, 28.02/1024
+# or that xx.xx.xxxx never contains anything beyond 12 in the first two fields,
+# so it's impossible to tell which is which - I suspect everything beyond 12 was removed
+# cannot test 04/05/09, because we can't tell which is which (d/m or m/d, both are used)
+
+# known good values:
+# 23-MAR-2006
+# 06-JAN-2006
+# Sep 25, 2012
+# 1996
+
+
+def test_parsing():
+    pass
