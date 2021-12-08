5254ef910b66ebd8f2c95e687d0455c301041cc4
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-12-08 14:27:04 +0100

    [icij]: wip

---
+++ data/icij/README.md
@@ -0,0 +1,3 @@
+https://offshoreleaks.icij.org/pages/database
+
+https://offshoreleaks-data.icij.org/offshoreleaks/csv/full-oldb-20211202.zip
---
+++ data/icij/schema.py
@@ -0,0 +1,43 @@
+from typing import Text
+from sqlalchemy import (
+    Table,
+    Column,
+    MetaData,
+)
+from sqlalchemy.sql.sqltypes import BigInteger, Integer, Numeric, Text
+
+meta = MetaData()
+
+schema = [
+    Table(
+        "zadatele",
+        meta,
+        Column("id_prijemce", BigInteger, nullable=False, primary_key=True),
+        Column("rok", Integer, nullable=False, primary_key=True),
+        Column("jmeno_nazev", Text, nullable=True),
+        Column("obec", Text, nullable=True),
+        Column("okres", Text, nullable=True),
+        Column("castka_bez_pvp", Numeric(12, 2), nullable=True),
+    ),
+    Table(
+        "platby",
+        meta,
+        Column("id_prijemce", BigInteger, nullable=False),
+        Column("rok", Integer, nullable=False),
+        Column("fond_typ_podpory", Text, nullable=True),
+        Column("opatreni", Text, nullable=True),
+        Column("zdroje_cr", Numeric(12, 2), nullable=True),
+        Column("zdroje_eu", Numeric(12, 2), nullable=True),
+        Column("celkem_czk", Numeric(12, 2), nullable=False),
+    ),
+]
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.schema import CreateTable
+
+    engine = create_engine("sqlite:///:memory:")
+    for table in schema:
+        print(f"-- {table.name} as created in SQLite")
+        print(CreateTable(table).compile(engine))
