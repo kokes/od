ae19b9949e23c0a73f199a8abb1cb985d4cc2ade
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-12-08 14:28:35 +0100

    [icij] load straight from icij.org

--- data/icij/main.py
+++ data/icij/main.py
@@ -1,28 +1,26 @@
 import os
+import shutil
 from contextlib import closing
 from tempfile import NamedTemporaryFile
 from urllib.request import urlopen
 from zipfile import ZipFile
 
-# TODO(PR): black, isort
-
 url = "https://offshoreleaks-data.icij.org/offshoreleaks/csv/full-oldb-20211202.zip"
 
-def main(outdir: str, partial: bool = False):
-    # TODO(PR): finish
-    # tmpf = NamedTemporaryFile()
-    # with closing(urlopen(url)) as rr:
-    #     shutil.copyfileobj(rr, tmpf)
 
-    # zf = ZipFile(tmpf.name)
-    fn = "full-oldb-20211202.zip"
-    zf = ZipFile(fn)
-    for member in zf.filelist:
-        zf.extract(member, path = outdir)
-        if member.filename.startswith("nodes-"):
-            target = os.path.join(outdir, member.filename)
-            new_target = target.replace("nodes-", "")
-            os.rename(target, new_target)
+def main(outdir: str, partial: bool = False):
+    tmpf = NamedTemporaryFile()
+    with closing(urlopen(url)) as rr:
+        shutil.copyfileobj(rr, tmpf)
+
+    with ZipFile(tmpf.name) as zf:
+        for member in zf.filelist:
+            zf.extract(member, path=outdir)
+            if member.filename.startswith("nodes-"):
+                target = os.path.join(outdir, member.filename)
+                new_target = target.replace("nodes-", "")
+                os.rename(target, new_target)
+
 
 if __name__ == "__main__":
     main(".")
