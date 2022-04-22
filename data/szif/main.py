56754187237ba0df3ec824cc0cbbf8f9437a071e
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-04-22 14:40:54 +0200

    [szif] zavirani zip souboru

--- data/szif/main.py
+++ data/szif/main.py
@@ -46,47 +46,46 @@ def main(outdir: str, partial: bool = False):
             tfn = os.path.join(tmpdir, "tmp.zip")
             urlretrieve(url, tfn)
 
-            zf = ZipFile(tfn)
+            with ZipFile(tfn) as zf:
+                assert len(zf.filelist) == 1, "Vic souboru nez ocekavano: {}".format(
+                    zf.filelist
+                )
 
-            assert len(zf.filelist) == 1, "Vic souboru nez ocekavano: {}".format(
-                zf.filelist
-            )
+                et = iterparse(zf.open(zf.filelist[0].filename))
 
-            et = iterparse(zf.open(zf.filelist[0].filename))
+                rok = None
+                for num, (action, element) in enumerate(et):
+                    if partial and num > 1e3:
+                        break
+                    assert action == "end"
+                    if element.tag == "rok":
+                        rok = int(element.text)
+                        assert rok == rok_ds, "Necekany rok v datech"
 
-            rok = None
-            for num, (action, element) in enumerate(et):
-                if partial and num > 1e3:
-                    break
-                assert action == "end"
-                if element.tag == "rok":
-                    rok = int(element.text)
-                    assert rok == rok_ds, "Necekany rok v datech"
+                    if element.tag != "zadatel":
+                        continue
 
-                if element.tag != "zadatel":
-                    continue
+                    zadatel = {"id_prijemce": id_prijemce, "rok": rok}
 
-                zadatel = {"id_prijemce": id_prijemce, "rok": rok}
+                    for key in ["jmeno_nazev", "obec", "okres", "castka_bez_pvp"]:
+                        zadatel[key] = element.find(key).text
 
-                for key in ["jmeno_nazev", "obec", "okres", "castka_bez_pvp"]:
-                    zadatel[key] = element.find(key).text
+                    for elplatba in element.findall("platby/platba") + element.findall(
+                        "platby_pvp/platba_pvp"
+                    ):
+                        platba = {"id_prijemce": id_prijemce, "rok": rok}
+                        for key in [
+                            "fond_typ_podpory",
+                            "opatreni",
+                            "zdroje_cr",
+                            "zdroje_eu",
+                            "celkem_czk",
+                        ]:
+                            platba[key] = getattr(elplatba.find(key), "text", None)
 
-                for elplatba in element.findall("platby/platba") + element.findall(
-                    "platby_pvp/platba_pvp"
-                ):
-                    platba = {"id_prijemce": id_prijemce, "rok": rok}
-                    for key in [
-                        "fond_typ_podpory",
-                        "opatreni",
-                        "zdroje_cr",
-                        "zdroje_eu",
-                        "celkem_czk",
-                    ]:
-                        platba[key] = getattr(elplatba.find(key), "text", None)
+                        cp.writerow(platba)
 
-                    cp.writerow(platba)
+                    cz.writerow(zadatel)
+                    id_prijemce += 1
 
-                cz.writerow(zadatel)
-                id_prijemce += 1
-
-                element.clear()
+                    element.clear()
