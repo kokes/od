ce14d2500584f13ccda8492a4171be1954e68011
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-04-22 14:59:34 +0200

    dalsi szif pokus

--- data/szif/main.py
+++ data/szif/main.py
@@ -51,41 +51,43 @@ def main(outdir: str, partial: bool = False):
                     zf.filelist
                 )
 
-                et = iterparse(zf.open(zf.filelist[0].filename))
+                with zf.open(zf.filelist[0].filename) as member:
+                    et = iterparse(member)
 
-                rok = None
-                for num, (action, element) in enumerate(et):
-                    if partial and num > 1e3:
-                        break
-                    assert action == "end"
-                    if element.tag == "rok":
-                        rok = int(element.text)
-                        assert rok == rok_ds, "Necekany rok v datech"
+                    rok = None
+                    for num, (action, element) in enumerate(et):
+                        if partial and num > 1e3:
+                            break
+                        assert action == "end"
+                        if element.tag == "rok":
+                            rok = int(element.text)
+                            assert rok == rok_ds, "Necekany rok v datech"
 
-                    if element.tag != "zadatel":
-                        continue
+                        if element.tag != "zadatel":
+                            continue
 
-                    zadatel = {"id_prijemce": id_prijemce, "rok": rok}
+                        zadatel = {"id_prijemce": id_prijemce, "rok": rok}
 
-                    for key in ["jmeno_nazev", "obec", "okres", "castka_bez_pvp"]:
-                        zadatel[key] = element.find(key).text
+                        for key in ["jmeno_nazev", "obec", "okres", "castka_bez_pvp"]:
+                            zadatel[key] = element.find(key).text
 
-                    for elplatba in element.findall("platby/platba") + element.findall(
-                        "platby_pvp/platba_pvp"
-                    ):
-                        platba = {"id_prijemce": id_prijemce, "rok": rok}
-                        for key in [
-                            "fond_typ_podpory",
-                            "opatreni",
-                            "zdroje_cr",
-                            "zdroje_eu",
-                            "celkem_czk",
-                        ]:
-                            platba[key] = getattr(elplatba.find(key), "text", None)
+                        for elplatba in (
+                            element.findall("platby/platba") +
+                            element.findall("platby_pvp/platba_pvp")
+                        ):
+                            platba = {"id_prijemce": id_prijemce, "rok": rok}
+                            for key in [
+                                "fond_typ_podpory",
+                                "opatreni",
+                                "zdroje_cr",
+                                "zdroje_eu",
+                                "celkem_czk",
+                            ]:
+                                platba[key] = getattr(elplatba.find(key), "text", None)
 
-                        cp.writerow(platba)
+                            cp.writerow(platba)
 
-                    cz.writerow(zadatel)
-                    id_prijemce += 1
+                        cz.writerow(zadatel)
+                        id_prijemce += 1
 
-                    element.clear()
+                        element.clear()
