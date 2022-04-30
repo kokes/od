6b7a9d7e7eaa756206f0c7e92f2060ba0891f547
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-04-30 17:20:05 +0200

    [zakazky] migrace z csv na xml (#178)
    resolves #166

--- data/zakazky/main.py
+++ data/zakazky/main.py
@@ -7,42 +7,30 @@ import gzip
 import json
 import os
 import re
-import shutil
 import ssl
 from contextlib import contextmanager
 from datetime import datetime
-from tempfile import TemporaryDirectory
 from urllib.request import Request, urlopen
+
+from lxml.etree import iterparse
 
 # ISVZ nema duveryhodny certy
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def najdi_typy(hd, typy):
-    ind = dict()  # {'date': [15, 22], 'numeric': [7, 8, 9]}
-    for k, v in typy.items():
-        ind[k] = []
-
-        for cl in v:
-            try:
-                j = hd.index(cl)
-                ind[k].append(j)
-            except ValueError:
-                pass
-
-    return ind
-
-
 dtpt = re.compile(r"^\d{1,2}\.\d{1,2}\.\d{4}$")
-isodate = re.compile(r"[0-9]{4}-[0-9]{2}-[0-9]{2}")
+isodate = re.compile(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
+isodatetime = re.compile(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}$")
 
 
 def fix_date(s):
-    if len(s) == 0:
+    if s is None or len(s) == 0:
         return None
 
     if isodate.match(s) is not None:
         return dt.date.fromisoformat(s).isoformat()
+    if isodatetime.match(s) is not None:
+        return dt.datetime.fromisoformat(s).isoformat()
 
     if dtpt.match(s) is not None:
         d, m, y = map(int, s.split("."))
@@ -51,15 +39,9 @@ def fix_date(s):
         return datetime.strptime(s, "%d.%m.%Y %H:%M:%S").isoformat()
 
 
-def fix_numeric(s):
-    if len(s) == 0:
-        return None
-    return float(s.replace(",", "."))
-
-
 # '000 23 234' - takhle se obcas zadavaj ICO
 def fix_ico(s):
-    if len(s) == 0:
+    if s is None or len(s) == 0:
         return None
     elif s.isdigit():
         rv = int(s)
@@ -69,41 +51,34 @@ def fix_ico(s):
         try:
             rv = int(s.replace(" ", "").replace("\xa0", ""))
         except ValueError:
-            print("nevalidni ICO", s)
             return None
 
     if rv < 100 * 10**6:
         return rv
     else:
-        print("ICO overflow", rv)
         return None
 
 
 @contextmanager
 def read_url(url):
     request = Request(url, headers={"Accept-Encoding": "gzip"})
-    with TemporaryDirectory() as tdir:
-        tfn = os.path.join(tdir, "data")
-        with urlopen(request, timeout=60) as r:
-            assert r.headers.get("Content-Encoding") == "gzip"
-            with gzip.open(r) as gr, open(tfn, "wb") as fw:
-                shutil.copyfileobj(gr, fw)
-        with open(tfn, encoding="utf-8-sig") as f:
-            yield f
+    with urlopen(request, timeout=60) as r:
+        assert r.headers.get("Content-Encoding") == "gzip"
+        yield gzip.open(r)
 
 
 root_url = "https://isvz.nipez.cz/sites/default/files/content/opendata-predchozi/"
 url_sources = {
     "zzvz": (
-        root_url + "ODZZVZ/{}.csv",
-        list(range(2016, 2021 + 1)),
+        root_url + "ODZZVZ/{}.xml",
+        list(range(2016, 2022 + 1)),
     ),
     "vvz": (
-        root_url + "ODVVZ/{}.csv",
+        root_url + "ODVVZ/{}.xml",
         list(range(2006, 2016 + 1)),
     ),
     "etrziste": (
-        root_url + "ODET/{}.csv",
+        root_url + "ODET/{}.xml",
         list(range(2012, 2017 + 1)),
     ),
 }
@@ -117,70 +92,60 @@ def main(outdir: str, partial: bool = False):
     assert list(allmaps.keys()) == ["etrziste", "vvz", "zzvz"]
 
     for ds, mapping in allmaps.items():
-        print(ds)
-        tblmap = {tuple(v): k for k, v in mapping["tabulky"].items()}
+        filehandles, csvwriters = {}, {}
 
-        for k, v in tblmap.items():
-            tfn = os.path.join(outdir, f"{ds}_{v}.csv")
-            with open(tfn, "w", encoding="utf8") as fw:
-                cw = csv.writer(fw, lineterminator="\n")
-                cw.writerow(k)
+        for v in mapping.values():
+            full_ds = f"{ds}_{v['table']}"
+            tfn = os.path.join(outdir, f"{full_ds}.csv")
+            filehandles[full_ds] = open(tfn, "w", encoding="utf8")
+            # TODO: museli jsme vypnout DictWriter, protoze ZZVZ jsou garbage
+            # pro roky 2021 a 2022
+            csvwriters[full_ds] = csv.writer(
+                filehandles[full_ds],
+                # fieldnames=v["header"],
+                lineterminator="\n",
+            )
+            # csvwriters[full_ds].writeheader()
+            csvwriters[full_ds].writerow(v["header"])
 
         base_url, years = url_sources[ds]
 
         for year in years:
-            # v `partial` procesnem jen posledni rok - nemuzem procesovat casti
-            # souboru, protoze co soubor, to nekolik datasetu
-            # TODO: pouzivam predposledni rok, protoze MMR nechce opravit
-            # posledni soubor v jednom z datasetu
-            if partial and year != years[-2]:
+            if partial and year != years[-1]:
                 continue
+            print(ds, year)
             url = base_url.format(year)
             with read_url(url) as resp:
-                cr = csv.reader(resp, delimiter=";")
-
-                for nrow, ln in enumerate(cr):
-                    if (len(ln) > 0 and ln[0] == mapping["hlavicka"]) or nrow == 0:
-                        if ln[0] == mapping["hlavicka"]:
-                            assert len(next(cr)) == 0  # prazdny radek po hlavicce
-                            hd = tuple(next(cr))
-                        else:
-                            # od 2021 nemaj některý soubory hlavičku
-                            hd = tuple(ln)
-                        tpmap = najdi_typy(hd, mapping["typy"])
-                        tp = tblmap[hd]  # document type
-                        f = open(
-                            os.path.join(outdir, f"{ds}_{tp}.csv"), "a", encoding="utf8"
-                        )
-                        cw = csv.writer(f, lineterminator="\n")
+                for action, element in iterparse(resp):
+                    assert action == "end"
+                    if element.tag not in mapping:
                         continue
+                    mp = mapping[element.tag]
+                    full_ds = f"{ds}_{mp['table']}"
 
-                    if len(ln) == 0:
-                        continue
+                    row = {
+                        el.tag: el.text.strip() if el.text else None
+                        for el in element.getchildren()
+                    }
 
-                    # TODO: az to opravi, tak tu exituj
-                    if len(hd) != len(ln):
-                        print(f"Necekane dlouha radka ({nrow}. v {url})")
-                        continue
-                        # with open("errs.csv", "a+") as ffw:
-                        #     ccw = csv.writer(ffw, lineterminator="\n")
-                        #     ccw.writerow(ln)
-                        # continue
+                    for k, v in row.items():
+                        if k in mp.get("dates", []):
+                            row[k] = fix_date(v)
+                        if v and k in mp.get("numeric", []):
+                            row[k] = v.replace(",", ".")
+                        if "ICO" in k:
+                            ico = fix_ico(v)
+                            if ico is None and v is not None:
+                                print("nevalidni ico", v, f"({full_ds}, {url})")
+                            row[k] = ico
 
-                    for k, v in tpmap.items():
-                        for cln in v:
-                            if k == "date":
-                                ln[cln] = fix_date(ln[cln])
-                            elif k == "numeric":
-                                ln[cln] = fix_numeric(ln[cln])
-                            elif k == "ico":
-                                ln[cln] = fix_ico(ln[cln])
-                            else:
-                                raise ValueError(k)
+                    # TODO: `writerow(row)` az prejdem zpet na DictWriter
+                    csvwriters[full_ds].writerow(row.values())
 
-                    cw.writerow(ln)
+                    element.clear()
 
-        f.close()
+        for fh in filehandles.values():
+            fh.close()
 
 
 if __name__ == "__main__":
