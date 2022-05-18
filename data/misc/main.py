a92f3af502d64c58d80b3001d159460d425d60e3
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-05-18 16:50:03 +0200

    nit

--- data/misc/main.py
+++ data/misc/main.py
@@ -7,7 +7,6 @@ URL_ODSOUZENE_PO = "https://eservice-po.rejtr.justice.cz/public/odsouzeni_csv"
 
 
 def main(outdir: str, partial: bool = False):
-    # urlretrieve(URL_ODSOUZENE_PO, os.path.join(outdir, "odsouzene_po.csv"))
     with urlopen(URL_ODSOUZENE_PO) as r, open(
         os.path.join(outdir, "odsouzene_po.csv"), "w", encoding="utf-8"
     ) as fw:
