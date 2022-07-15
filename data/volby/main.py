b043ba2cebbcc8b0bb686f0b5d29aad6890f813f
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-07-15 12:13:25 +0200

    [volby] paralelizace exekuce (#205)

--- data/volby/main.py
+++ data/volby/main.py
@@ -1,11 +1,15 @@
 import csv
+import functools
 import json
+import multiprocessing
 import os
 import shutil
 import zipfile
+from collections import defaultdict
 from contextlib import contextmanager
 from fnmatch import fnmatch
 from tempfile import TemporaryDirectory
+from urllib.parse import urlparse
 from urllib.request import Request, urlopen
 
 import lxml.etree
@@ -14,17 +18,16 @@ from dbfread import DBF
 
 @contextmanager
 def load_remote_data(url: str):
-    raw_dir = "data/raw"
-    os.makedirs(raw_dir, exist_ok=True)
-    fn = os.path.basename(url)
-    tfn = os.path.join(raw_dir, fn)
-    if not os.path.isfile(tfn):
-        req = Request(url, headers={"User-Agent": "https://github.com/kokes/od"})
-        with urlopen(req, timeout=60) as r, open(tfn, "wb") as fw:
-            shutil.copyfileobj(r, fw)
+    with TemporaryDirectory() as tmpdir:
+        fn = os.path.basename(url)
+        tfn = os.path.join(tmpdir, fn)
+        if not os.path.isfile(tfn):
+            req = Request(url, headers={"User-Agent": "https://github.com/kokes/od"})
+            with urlopen(req, timeout=60) as r, open(tfn, "wb") as fw:
+                shutil.copyfileobj(r, fw)
 
-    with zipfile.ZipFile(tfn) as zf:
-        yield zf
+        with zipfile.ZipFile(tfn) as zf:
+            yield zf
 
 
 def extract_elements(zf, fn, nodename):
@@ -53,79 +56,82 @@ def extract_elements(zf, fn, nodename):
         raise NotImplementedError(fn)
 
 
+def process_url(outdir, partial, fnmap, url: str, volby: str, datum: str):
+    with load_remote_data(url) as zf:
+        for ff in map(lambda x: x.filename, zf.filelist):
+            patterns = [j for j in fnmap[volby].keys() if fnmatch(ff, j)]
+            if len(patterns) == 0:
+                continue
+            if len(patterns) > 1:
+                raise KeyError("ambiguous keys: {}".format(patterns))
+
+            ds, fmp = fnmap[volby].get(patterns[0])
+            tdir = os.path.join(outdir, f"{volby}_{ds}")
+            os.makedirs(tdir, exist_ok=True)
+            url_path = os.path.splitext(os.path.basename(urlparse(url).path))[0]
+            tfn = os.path.join(
+                tdir, f"{datum}_{url_path}_{os.path.splitext(ff)[0]}.csv"
+            )
+            if os.path.isfile(tfn):
+                raise IOError(f"necekany prepis souboru: {tfn}")
+            with open(tfn, "wt", encoding="utf8") as fw:
+                cw = csv.DictWriter(
+                    fw,
+                    fieldnames=["DATUM"] + fmp["schema"] + fmp.get("extra_schema", []),
+                    lineterminator="\n",
+                )
+                for ne, el in enumerate(extract_elements(zf, ff, fmp["klic"])):
+                    if partial and ne > 1e4:
+                        break
+                    for k in fmp.get("vynechej", []):
+                        el.pop(k, None)
+
+                    # TODO: TEST: HLASY_01 vs. HLASY_K1
+                    hk = [
+                        k
+                        for k in el.keys()
+                        if k.startswith("HLASY_") and k.partition("_")[-1].isdigit()
+                    ]
+                    if hk:
+                        hlasy = []
+                        for k in hk:
+                            hlasy.append(el[k] or 0)
+                            del el[k]
+
+                        # pg array representation - '{a, b, c}'
+                        el["HLASY"] = "{{{}}}".format(",".join(map(str, hlasy)))
+
+                    cw.writerow(
+                        {
+                            "DATUM": datum,
+                            **el,
+                        }
+                    )
+
+
 def main(outdir: str, partial: bool = False):
     cdir = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(cdir, "mapping.json"), encoding="utf-8") as f:
         mps = json.load(f)
 
+    ncpu = multiprocessing.cpu_count()
+    jobs = []
+    fnmap = defaultdict(dict)
     for volby, mp in mps.items():
-        print(volby)
-
-        fnmap = {}
         for ds, spec in mp["ds"].items():
             for fn in spec["fn"]:
-                fnmap[fn] = (ds, spec)
+                assert fn not in fnmap, fn
+                fnmap[volby][fn] = (ds, spec)
 
         for datum, urls in mp["url"].items():
             if partial and datum != sorted(mp["url"].keys())[-1]:
                 continue
-            print("\t", datum)
             for url in urls:
-                with load_remote_data(url) as zf:
-                    for ff in map(lambda x: x.filename, zf.filelist):
-                        patterns = [j for j in fnmap.keys() if fnmatch(ff, j)]
-                        if len(patterns) == 0:
-                            continue
-                        if len(patterns) > 1:
-                            raise KeyError("ambiguous keys: {}".format(patterns))
+                jobs.append((url, volby, datum))
 
-                        ds, fmp = fnmap.get(patterns[0])
-                        tdir = os.path.join(outdir, f"{volby}_{ds}")
-                        os.makedirs(tdir, exist_ok=True)
-                        tfn = os.path.join(tdir, f"{datum}.csv")
-                        fnexists = os.path.isfile(tfn)
-                        with open(tfn, "a+", encoding="utf8") as fw:
-                            cw = csv.DictWriter(
-                                fw,
-                                fieldnames=["DATUM"]
-                                + fmp["schema"]
-                                + fmp.get("extra_schema", []),
-                                lineterminator="\n",
-                            )
-                            if not fnexists:
-                                cw.writeheader()
-                            for ne, el in enumerate(
-                                extract_elements(zf, ff, fmp["klic"])
-                            ):
-                                if partial and ne > 1e4:
-                                    break
-                                for k in fmp.get("vynechej", []):
-                                    el.pop(k, None)
-
-                                # TODO: TEST: HLASY_01 vs. HLASY_K1
-                                hk = [
-                                    k
-                                    for k in el.keys()
-                                    if k.startswith("HLASY_")
-                                    and k.partition("_")[-1].isdigit()
-                                ]
-                                if hk:
-                                    hlasy = []
-                                    for k in hk:
-                                        hlasy.append(el[k] or 0)
-                                        del el[k]
-
-                                    # pg array representation - '{a, b, c}'
-                                    el["HLASY"] = "{{{}}}".format(
-                                        ",".join(map(str, hlasy))
-                                    )
-
-                                cw.writerow(
-                                    {
-                                        "DATUM": datum,
-                                        **el,
-                                    }
-                                )
+    job_processor = functools.partial(process_url, outdir, partial, fnmap)
+    with multiprocessing.Pool(ncpu) as pool:
+        pool.starmap(job_processor, jobs)
 
 
 if __name__ == "__main__":
