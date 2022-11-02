9193f73a138c49496f9ca02d0c8409345ab27fa7
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-11-02 15:05:53 +0100

    dedentuj db práci

--- main.py
+++ main.py
@@ -106,133 +106,131 @@ if __name__ == "__main__":
             module(outdir_tmp, partial=args.partial)
             os.rename(outdir_tmp, outdir)
 
-        if engine:
-            table_loads = defaultdict(list)
-            for table in schemas[module_name]:
-                fn_cand = os.path.join(outdir, table.name + ".csv")
-                dir_cand = os.path.join(outdir, table.name)
-                if os.path.isfile(fn_cand):
-                    table_loads[(module_name, table.name)].append(fn_cand)
-                elif os.path.isdir(dir_cand):
-                    for basename in os.listdir(dir_cand):
-                        table_loads[(module_name, table.name)].append(
-                            os.path.join(dir_cand, basename)
-                        )
-                else:
-                    raise IOError(f"neexistujou data pro {module_name}.{table.name}")
+        if not engine:
+            continue
 
-            for table in schemas[module_name]:
-                t = time.time()
-                print(f"Nahravam {table.name} do {module_name}", end="")
-                files = table_loads[(module_name, table.name)]
-                fkeys = [
-                    j for j in table.constraints if isinstance(j, ForeignKeyConstraint)
-                ]
-
-                # základní kontrola integrity (oflagovat?)
-                # .lower() na obou stranach pro case insensitive porovnani
-                db_column_names = [j.name.lower() for j in table.columns]
-                db_column_nullable = [j.nullable for j in table.columns]
-                data_nullable = [False for _ in table.columns]
-                for file in files:
-                    with open(file, "rt", encoding="utf-8") as f:
-                        cr = csv.reader(f)
-                        header = [j.lower() for j in next(cr)]
-                        if header != db_column_names:
-                            errmap = dict(
-                                (k, v)
-                                for k, v in zip(header, db_column_names)
-                                if k != v
-                            )
-                            warnings.warn(f"databáze očekává jiné sloupce: {errmap}")
-
-                        for j, row in enumerate(cr):
-                            if len(row) != len(db_column_names):
-                                raise ValueError(
-                                    f"nečekaný počet sloupců, {len(row)} vs."
-                                    f" {len(db_column_names)} (řádka {j+2}"
-                                )
-                            for k, val in enumerate(row):
-                                if val == "":
-                                    data_nullable[k] = True
-
-                if data_nullable != db_column_nullable:
-                    for j, (dnull, dbnull) in enumerate(
-                        zip(data_nullable, db_column_nullable)
-                    ):
-                        if dnull == dbnull:
-                            continue
-                        warnings.warn(
-                            f"NULL neshoda v {table.name} ({db_column_names[j]}):"
-                            f" data ({dnull}) vs. DB ({dbnull})"
-                        )
-
-                if engine.name == "postgresql":
-                    table.schema = f"{args.schema_prefix}{module_name}"
-                    engine.execute(f"CREATE SCHEMA IF NOT EXISTS {table.schema}")
-                elif engine.name == "sqlite":
-                    table.name = f"{args.schema_prefix}{module_name}_{table.name}"
-
-                if args.drop_first:
-                    table.drop(engine, checkfirst=True)
-                table.create(engine, checkfirst=True)
-
-                # dropni fkeys pred nahravanim dat
-                # z nejakeho duvodu jsou v sqlite nepojmenovany klice
-                if engine.name == "postgresql":
-                    dbtable = Table(
-                        table.name, MetaData(engine), schema=table.schema, autoload=True
+        table_loads = defaultdict(list)
+        for table in schemas[module_name]:
+            fn_cand = os.path.join(outdir, table.name + ".csv")
+            dir_cand = os.path.join(outdir, table.name)
+            if os.path.isfile(fn_cand):
+                table_loads[(module_name, table.name)].append(fn_cand)
+            elif os.path.isdir(dir_cand):
+                for basename in os.listdir(dir_cand):
+                    table_loads[(module_name, table.name)].append(
+                        os.path.join(dir_cand, basename)
                     )
-                    for fk in dbtable.constraints:
-                        if isinstance(fk, ForeignKeyConstraint):
-                            DropConstraint(fk).execute(engine)
+            else:
+                raise IOError(f"neexistujou data pro {module_name}.{table.name}")
 
-                if engine.name == "postgresql":
-                    full_table_name = f"{table.schema}.{table.name}"
-                    conn = engine.raw_connection()
-                    cur = conn.cursor()
-                    cur.execute(
-                        f"TRUNCATE {full_table_name} CASCADE"
-                    )  # TODO: cascade yolo
-                    for filename in files:
-                        with open(filename, "rt", encoding="utf-8") as f:
-                            cur.copy_expert(
-                                f"COPY {full_table_name} FROM stdin WITH CSV HEADER", f
+        for table in schemas[module_name]:
+            t = time.time()
+            print(f"Nahravam {table.name} do {module_name}", end="")
+            files = table_loads[(module_name, table.name)]
+            fkeys = [
+                j for j in table.constraints if isinstance(j, ForeignKeyConstraint)
+            ]
+
+            # základní kontrola integrity (oflagovat?)
+            # .lower() na obou stranach pro case insensitive porovnani
+            db_column_names = [j.name.lower() for j in table.columns]
+            db_column_nullable = [j.nullable for j in table.columns]
+            data_nullable = [False for _ in table.columns]
+            for file in files:
+                with open(file, "rt", encoding="utf-8") as f:
+                    cr = csv.reader(f)
+                    header = [j.lower() for j in next(cr)]
+                    if header != db_column_names:
+                        errmap = dict(
+                            (k, v) for k, v in zip(header, db_column_names) if k != v
+                        )
+                        warnings.warn(f"databáze očekává jiné sloupce: {errmap}")
+
+                    for j, row in enumerate(cr):
+                        if len(row) != len(db_column_names):
+                            raise ValueError(
+                                f"nečekaný počet sloupců, {len(row)} vs."
+                                f" {len(db_column_names)} (řádka {j+2}"
                             )
-                    conn.commit()  # TODO: proc nejde context manager? starej psycopg?
-                elif engine.name == "sqlite":
-                    conn = engine.raw_connection()
-                    conn.execute(f"DELETE FROM {table.name}")  # truncate v sqlite neni
+                        for k, val in enumerate(row):
+                            if val == "":
+                                data_nullable[k] = True
 
-                    ph = ", ".join(["?"] * len(table.columns))
-                    query = f"INSERT INTO {table.name} VALUES({ph})"
-                    bools = [isinstance(j.type, Boolean) for j in table.columns]
-                    for filename in files:
-                        buffer = []
-                        with open(filename, "rt", encoding="utf-8") as f:
-                            cr = csv.reader(f)
-                            next(cr)  # header
-                            for row in cr:
-                                row = [
-                                    bool(el) if bools[j] and el != "" else el
-                                    for j, el in enumerate(row)
-                                ]
-                                row = [None if j == "" else j for j in row]
-                                buffer.append(row)
-                                if len(buffer) == 100:
-                                    conn.executemany(query, buffer)
-                                    buffer = []
-                            if len(buffer) > 0:
+            if data_nullable != db_column_nullable:
+                for j, (dnull, dbnull) in enumerate(
+                    zip(data_nullable, db_column_nullable)
+                ):
+                    if dnull == dbnull:
+                        continue
+                    warnings.warn(
+                        f"NULL neshoda v {table.name} ({db_column_names[j]}):"
+                        f" data ({dnull}) vs. DB ({dbnull})"
+                    )
+
+            if engine.name == "postgresql":
+                table.schema = f"{args.schema_prefix}{module_name}"
+                engine.execute(f"CREATE SCHEMA IF NOT EXISTS {table.schema}")
+            elif engine.name == "sqlite":
+                table.name = f"{args.schema_prefix}{module_name}_{table.name}"
+
+            if args.drop_first:
+                table.drop(engine, checkfirst=True)
+            table.create(engine, checkfirst=True)
+
+            # dropni fkeys pred nahravanim dat
+            # z nejakeho duvodu jsou v sqlite nepojmenovany klice
+            if engine.name == "postgresql":
+                dbtable = Table(
+                    table.name, MetaData(engine), schema=table.schema, autoload=True
+                )
+                for fk in dbtable.constraints:
+                    if isinstance(fk, ForeignKeyConstraint):
+                        DropConstraint(fk).execute(engine)
+
+            if engine.name == "postgresql":
+                full_table_name = f"{table.schema}.{table.name}"
+                conn = engine.raw_connection()
+                cur = conn.cursor()
+                cur.execute(f"TRUNCATE {full_table_name} CASCADE")  # TODO: cascade yolo
+                for filename in files:
+                    with open(filename, "rt", encoding="utf-8") as f:
+                        cur.copy_expert(
+                            f"COPY {full_table_name} FROM stdin WITH CSV HEADER", f
+                        )
+                conn.commit()  # TODO: proc nejde context manager? starej psycopg?
+            elif engine.name == "sqlite":
+                conn = engine.raw_connection()
+                conn.execute(f"DELETE FROM {table.name}")  # truncate v sqlite neni
+
+                ph = ", ".join(["?"] * len(table.columns))
+                query = f"INSERT INTO {table.name} VALUES({ph})"
+                bools = [isinstance(j.type, Boolean) for j in table.columns]
+                for filename in files:
+                    buffer = []
+                    with open(filename, "rt", encoding="utf-8") as f:
+                        cr = csv.reader(f)
+                        next(cr)  # header
+                        for row in cr:
+                            row = [
+                                bool(el) if bools[j] and el != "" else el
+                                for j, el in enumerate(row)
+                            ]
+                            row = [None if j == "" else j for j in row]
+                            buffer.append(row)
+                            if len(buffer) == 100:
                                 conn.executemany(query, buffer)
-                    conn.commit()
-                else:
-                    raise IOError(f"{engine.name} not supported yet")
+                                buffer = []
+                        if len(buffer) > 0:
+                            conn.executemany(query, buffer)
+                conn.commit()
+            else:
+                raise IOError(f"{engine.name} not supported yet")
 
-                # constrainty jsme neumeli dropnout u sqlite... a nejdou ani pridat
-                if engine.name == "postgresql":
-                    for fk in fkeys:
-                        if not isinstance(fk, ForeignKeyConstraint):
-                            continue
-                        AddConstraint(fk).execute(bind=engine)
+            # constrainty jsme neumeli dropnout u sqlite... a nejdou ani pridat
+            if engine.name == "postgresql":
+                for fk in fkeys:
+                    if not isinstance(fk, ForeignKeyConstraint):
+                        continue
+                    AddConstraint(fk).execute(bind=engine)
 
-                print(f" ({time.time() - t:.2f}s)")
+            print(f" ({time.time() - t:.2f}s)")
