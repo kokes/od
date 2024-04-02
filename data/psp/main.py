f839b97b056aab2bbd4225786ee7abb7b6cba131
Ondrej Kokes <ondrej.kokes@gmail.com>
2024-04-02 06:59:36 +0200

    unverified ssl

--- data/psp/main.py
+++ data/psp/main.py
@@ -8,12 +8,15 @@ import logging
 import multiprocessing
 import os
 import shutil
+import ssl
 import zipfile
 from contextlib import contextmanager
 from datetime import datetime
 from io import TextIOWrapper
 from tempfile import TemporaryDirectory
 from urllib.request import urlopen
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 NULL_DATE = datetime(1900, 1, 1, 0, 0)
 HTTP_TIMEOUT = 90
