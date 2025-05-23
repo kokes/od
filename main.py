6e5977511d61315da71e6e608c099963892239e5
Ondrej Kokes <ondrej.kokes@gmail.com>
2025-05-23 09:43:10 +0200

    black

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def main(
 ):
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     logging.info("Modul %s", module_name)
 
