a15616764bba0a2878eb1939071bb60df8ba4506
Ondrej Kokes <ondrej.kokes@gmail.com>
2025-06-17 14:11:52 +0200

    http client v testech

--- server/tests/test_status.py
+++ server/tests/test_status.py
@@ -1,5 +1,5 @@
 import json
-from urllib.request import urlopen
+from http.client import HTTPConnection
 
 import pytest
 
@@ -12,19 +12,18 @@ TEST_DB = "data.sqlite"  # TODO: priprav v ramci testu
 @pytest.fixture(scope="module")
 def server():
     httpd, thread = run_server_in_thread(TEST_DB, PORT)
-    yield f"http://{httpd.server_address[0]}:{httpd.server_address[1]}"
+    conn = HTTPConnection("localhost", PORT)
+
+    yield conn
+
+    conn.close()
     httpd.shutdown()
     thread.join()
 
 
 def test_status(server):
-    with urlopen(f"{server}/status") as response:
-        assert response.status == 200
-        data = json.load(response)
-        assert data == {"status": "ok"}
-
-
-# def test_status(client):
-#     response = client.get("/status")
-#     assert response.status_code == 200
-#     assert response.data == b'{"status":"ok"}\n'
+    server.request("GET", "/status")
+    response = server.getresponse()
+    assert response.status == 200
+    data = json.load(response)
+    assert data == {"status": "ok"}
